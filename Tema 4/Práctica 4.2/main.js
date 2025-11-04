aecb13d2a3010826962c452c093eea2edb747ad9
Jaime Vargas <jvarbae782@g.educaand.es>
2025-11-04 10:30:51 +0100

    practica 4.2 terminada

---
+++ Tema 4/Práctica 4.2/main.js
@@ -0,0 +1,19 @@
+let a = document.getElementById("platform");
+let table = document.getElementsByClassName("tabla");
+
+a.addEventListener('mouseenter', () =>{
+    table[0].style.display="block";
+});
+
+table[0].addEventListener('mouseleave', () => {
+    table[0].style.display="none";
+}); 
+
+a.addEventListener('mouseleave', () => {
+  setTimeout(() => {
+    if (!table[0].matches(':hover')) {
+      table[0].style.display = "none";
+    }
+  }, 100); // pequeño delay para permitir entrar en la tabla
+}); 
+
